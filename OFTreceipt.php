f89f63f6ca8024f9e5d43b3d6032e9e062f6b528
spatialneuroscience <51832402+spatialneuroscience@users.noreply.github.com>
2020-06-13 19:57:36 -0700

    Add files via upload
    Complete OFT receipt PHP call. No need for OFTreceipt_obj.php

--- OFTreceipt.php
+++ OFTreceipt.php
@@ -11,4 +11,17 @@
 	{
 		echo("Messsage delivery failed...");
 	}
+
+	$text2 = $_POST["input2"];
+	$filename2 = $_POST["filename2"];
+
+	if ($text2 != "")
+	{
+		$file2 = fopen($filename2, "a"); 
+		fwrite($file2, $text2);
+		fclose($file2);
+	} else
+	{
+		echo("Messsage delivery failed...");
+	}
 ?>
